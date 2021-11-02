8b8543c4d67c6266c4eaa2746ef321caa82ae1ae
Vimal Patel <vimalp@itpathsolutions.in>
2021-11-02 13:01:27 +0530

    ff

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const serverless = require('serverless-http')
 var cors = require('cors');
 
 const options = {
-    key: fs.readFileSync('server/key.pem', 'utf8'),
-    cert: fs.readFileSync('server/cert.pem', 'utf8')
+  key: fs.readFileSync('server/key.pem', 'utf8'),
+  cert: fs.readFileSync('server/cert.pem', 'utf8')
 };
 
-var credentials = {key: options.key, cert: options.cert};
+var credentials = { key: options.key, cert: options.cert };
 
 const express = require('express');
 var https = require('https');
@@ -23,23 +24,23 @@ const VideoGrant = AccessToken.VideoGrant;
 const app = express();
 
 app.use(cors());
-var server = https.createServer(credentials, app);
+// var server = https.createServer(credentials, app);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 app.get('/', (request, response) => {
   console.log("server running");
   response.status(200).send("helloo");
 });
 
-app.post('/token',jsonParser, function(request, response) {
+app.post('/token', jsonParser, function (request, response) {
   const { identity, room_name } = request.body;
   const token = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_KEY,
     process.env.TWILIO_API_KEY_SECRET,
-    {identity: identity},
-    {rules: [{"type": "exclude", "all": true}]}
+    { identity: identity },
+    { rules: [{ "type": "exclude", "all": true }] }
   );
   const grant = new VideoGrant({
     room: room_name,
@@ -48,20 +49,18 @@ app.post('/token',jsonParser, function(request, response) {
   response.send(token.toJwt());
 });
 
-app.post('/recording',jsonParser, (request, response) => {
+app.post('/recording', jsonParser, (request, response) => {
 
-  const { rules,room } = request.body;
+  const { rules, room } = request.body;
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const clientAPI = require('twilio')(accountSid, authToken);
 
   clientAPI.video.rooms(room)
-              .recordingRules
-              .update({rules: rules})
-              .then(recording_rules => console.log(recording_rules.roomSid));
+    .recordingRules
+    .update({ rules: rules })
+    .then(recording_rules => console.log(recording_rules.roomSid));
   response.send(true);
 });
 
-server.listen(port, function() {
-  console.log('Express server running on *:' + port);
-});
+module.exports.handler = serverless(app);
